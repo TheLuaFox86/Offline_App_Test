5619ade55a3bb04ce378849e30c0eb4705c67c79
= <theluafox86yt@gmail.com>
2026-08-04 19:46:33 -0400

    Auto Generated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Offline_App_Test/',
   plugins: [VitePWA({
     registerType: 'prompt',
     injectRegister: false,
