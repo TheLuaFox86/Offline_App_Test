b0749c73f8146f73105257b9242af687f7278047
= <theluafox86yt@gmail.com>
2026-08-04 18:10:19 -0400

    im still learning

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // WARNING: Must match your exact repository name case-sensitively
+  base: '/Offline_App_Test/', 
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // Include any other PWA configuration properties here
+    })
+  ]
 })
+
