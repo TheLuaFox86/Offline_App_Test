e9d1961c54e496ebbd6035a07ba080077cbff687
= <theluafox86yt@gmail.com>
2026-08-04 19:38:55 -0400

    Auto Generated

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,35 @@
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // WARNING: Must match your exact repository name case-sensitively
-  base: '/Offline_App_Test/', 
-  plugins: [
-    react(),
-   // VitePWA({
-     // registerType: 'autoUpdate',
-      // Include any other PWA configuration properties here
-    //})
-  ]
+  plugins: [VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'my-offline-app',
+      short_name: 'my-offline-app',
+      description: 'my-offline-app',
+      theme_color: '#3C0000',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
 })
